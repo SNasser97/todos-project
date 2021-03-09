21ade6018c1e1eb928930d4af5938316e0edd298
SNasser97 <snasserahmed@outlook.com>
2021-03-09 17:19:50 +0000

    Removed UpdateTodoCommand update property

--- src/todos-logic/Todos/Command/UpdateTodoCommand.cs
+++ src/todos-logic/Todos/Command/UpdateTodoCommand.cs
@@ -6,19 +6,17 @@ namespace todos_logic.Todos.Command
     public class UpdateTodoCommand
     {
         public Guid Id { get; set; }
+        
         public string Name { get; set; }
 
         public bool IsComplete { get; set; }
 
-        public long UpdatedAt { get; set; }
-        
         public Todo ToTodoData()
         {
             return new Todo
             {
                 Id = this.Id,
                 Name = this.Name,
-                UpdatedAt = this.UpdatedAt,
                 IsComplete = this.IsComplete
             };
         }
